d26ceac726eaae8a161d26779ff0e481c4632759
UnknownIsaac <isaacsims100@gmail.com>
2023-05-04 18:04:36 +0100

    Ventanas Programacion

--- src/main/java/com/example/prueba2/Carga_Pantalla.java
+++ src/main/java/com/example/prueba2/Carga_Pantalla.java
@@ -9,8 +9,10 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import org.w3c.dom.Text;
 
 import java.io.IOException;
 import java.net.URL;
@@ -19,10 +21,10 @@ import java.util.ResourceBundle;
 public class Carga_Pantalla implements Initializable {
 
 @FXML
-private Button btnRegis;
+private TextField txtCorreo;
 @FXML
-private Button btnEntrar;
-
+private TextField txtContraseña;
+//https://www.youtube.com/watch?v=I04VACXbYbA
 
 @FXML
     private void Regis(ActionEvent event) {
@@ -83,9 +85,82 @@ private Button btnEntrar;
     @FXML
     private void Volver (ActionEvent event){
 
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("Inicio.fxml"));
 
+            // Cargo la ventana
+            Parent root = loader.load();
+
+            // Cojo el controlador
+            Carga_Pantalla controlador = loader.getController();
+
+            // Creo el Scene
+            Scene scene = new Scene(root);
+            Stage stage = new Stage();
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.setScene(scene);
+            stage.showAndWait();
+        } catch (IOException ex) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setHeaderText(null);
+            alert.setTitle("Error");
+            alert.setContentText(ex.getMessage());
+            alert.showAndWait();
+        }
+    }
+@FXML
+private void Rentar(ActionEvent event) {
+    try {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("Compra.fxml"));
+
+        // Cargo la ventana
+        Parent root = loader.load();
+
+        // Cojo el controlador
+        Carga_Pantalla controller = loader.getController();
+
+        // Creo el Scene
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.setScene(scene);
+        stage.showAndWait();
+    }catch (IOException ex) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setHeaderText(null);
+        alert.setTitle("Error");
+        alert.setContentText(ex.getMessage());
+        alert.showAndWait();
     }
 
+}
+
+@FXML
+private void Pagar(ActionEvent event) {
+    try {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("Felicidades.fxml"));
+
+        // Cargo la ventana
+        Parent root = loader.load();
+
+        // Cojo el controlador
+        Carga_Pantalla controller = loader.getController();
+
+        // Creo el Scene
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.setScene(scene);
+        stage.showAndWait();
+    }catch (IOException ex) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setHeaderText(null);
+        alert.setTitle("Error");
+        alert.setContentText(ex.getMessage());
+        alert.showAndWait();
+
+}
+}
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
--- src/main/java/com/example/prueba2/HelloApplication.java
+++ src/main/java/com/example/prueba2/HelloApplication.java
@@ -11,9 +11,12 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Inicio.fxml"));
+        //Parent root = loader.load();
         Scene scene = new Scene(fxmlLoader.load(), 400, 400);
         stage.setTitle("Inicio de Sesión");
         stage.setScene(scene);
+        //Cargar_Pantalla controller = loader.getController();
+        //controller.setStage(stage);
         stage.show();
     }
 
